b8365d648ff2dcec4ae2eff55be0639f52433244
Amol Deshmukh <rushikeshdeshpande@AmolMacBook.local>
2014-08-06 13:05:49 +0530

    Initial commit

---
+++ sampleproject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  sampleproject
+//
+//  Created by Amol Deshmukh on 06/08/14.
+//  Copyright (c) 2014 Mindscrub Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
