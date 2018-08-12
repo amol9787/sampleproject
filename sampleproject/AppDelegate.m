077ab38192e37744beb001c319cf39e2f2f29260
AmolD <amoldeshmukh87@gmail.com>
2018-08-12 22:05:43 +0530

    This is master commit 2 from local

--- sampleproject/AppDelegate.m
+++ sampleproject/AppDelegate.m
@@ -28,6 +28,7 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+    printf("This is master commit 2 from local");
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
