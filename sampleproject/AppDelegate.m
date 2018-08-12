b92442eba4156983d399e383a97ff9bc6df6b048
amol9787 <amoldeshmukh87@gmail.com>
2018-08-12 22:01:40 +0530

    This is master commit 3 from remote"

--- sampleproject/AppDelegate.m
+++ sampleproject/AppDelegate.m
@@ -21,7 +21,7 @@
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    printf("This is master commit 2 from remote");
+    printf("This is master commit 3 from remote");
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
