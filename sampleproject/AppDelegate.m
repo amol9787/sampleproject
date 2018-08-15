221b39786dd8d5d605938cda531f971f76154ac7
AmolD <amoldeshmukh87@gmail.com>
2018-08-15 21:38:19 +0530

    This is fundflow update 3

--- sampleproject/AppDelegate.m
+++ sampleproject/AppDelegate.m
@@ -33,7 +33,7 @@
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    printf("This is fundflow branch");
+    printf("This is fundflow branch and update 3");
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
